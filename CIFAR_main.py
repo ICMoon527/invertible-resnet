fix zero gradients error

CIFAR_main.py
@@ -24,7 +24,7 @@ from models.conv_iResNet import conv_iResNet as iResNet
 from models.conv_iResNet import multiscale_conv_iResNet as multiscale_iResNet
 
 
-os.environ['CUDA_VISIBLE_DEVICES']='2, 3'
+# os.environ['CUDA_VISIBLE_DEVICES']='2, 3'
 # torch.backends.cudnn.enabled = False
 
 

matrix_utils.py
@@ -7,7 +7,7 @@ ICML, 2019
 import numpy as np
 import torch
 from scipy.linalg import logm
-from torch.autograd.gradcheck import zero_gradients
+# from torch.autograd.gradcheck import zero_gradients
 import torch.nn as nn
 
 def exact_matrix_logarithm_trace(Fx, x):
@@ -24,7 +24,7 @@ def exact_matrix_logarithm_trace(Fx, x):
     jac = torch.empty([bs, outdim, indim[1]], dtype=torch.float)
     # for each output Fx[i] compute d(Fx[i])/d(x)
     for i in range(outdim):
-        zero_gradients(x)
+        x.zero_grad()
         jac[:, i, :] = torch.autograd.grad(outVector[i], x,
                                            retain_graph=True)[0].view(bs, outdim)
     jac = jac.cpu().numpy()